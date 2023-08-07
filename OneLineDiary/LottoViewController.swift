f1369f365c2f34e3783de0ba991a68d9e73211b3
ji-yeon224 <69784492+ji-yeon224@users.noreply.github.com>
2023-08-07 12:03:02 +0900

    230807 수업 - pickerview를 통해 로또 당첨 번호 가져오기

--- OneLineDiary/LottoViewController.swift
+++ OneLineDiary/LottoViewController.swift
@@ -6,12 +6,16 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class LottoViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
     
     
     @IBOutlet var numberTextField: UITextField!
     
+    @IBOutlet var dateLabel: UILabel!
+    @IBOutlet var bonusNumberLabel: UILabel!
     let pickerView = UIPickerView() //생성
     
     
@@ -22,13 +26,50 @@ class LottoViewController: UIViewController, UIPickerViewDelegate, UIPickerViewD
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        print("1")
+        print("2")
+        
+        callRequest(number: 1079)
+        
+        
+        print("4")
+        
         numberTextField.inputView = pickerView
         numberTextField.tintColor = .clear
         
+        
+        
         pickerView.delegate = self
         pickerView.dataSource = self
+        
+        print("5")
 
     }
+    
+    func callRequest(number: Int) {
+        let url = "https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=\(number)" //url -> 요청 보낼 링크 넣기
+        AF.request(url, method: .get).validate().responseJSON { response in
+            switch response.result {
+            case .success(let value):
+                let json = JSON(value)
+                print("JSON: \(json)")
+                print("3")
+                
+                let date = json["drwNoDate"].stringValue //타입 명시
+                let bonusNumber = json["bnusNo"].intValue
+                
+                print(date, bonusNumber)
+                
+                self.dateLabel.text = date
+                self.bonusNumberLabel.text = "\(bonusNumber)번"
+                
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+    
+    
     
     //pickerview 분할
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
@@ -44,6 +85,9 @@ class LottoViewController: UIViewController, UIPickerViewDelegate, UIPickerViewD
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         print("select \(row)")
         numberTextField.text = "\(list[row])"
+        
+        callRequest(number: list[row])
+        
     }
     
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
