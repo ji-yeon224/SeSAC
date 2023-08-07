3b75623f2acf03f8a9c042383cdc99970ab893ce
ji-yeon224 <69784492+ji-yeon224@users.noreply.github.com>
2023-08-07 21:09:59 +0900

    json 계층에 따라 요소에 접근하기

--- Week4Project/ViewController.swift
+++ Week4Project/ViewController.swift
@@ -19,12 +19,14 @@ class ViewController: UIViewController {
     
     func callRequest() {
         
-        let url = "http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=f5eef3421c602c6cb7ea224104795888&targetDt=20120101"
+        let url = "http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=\(APIKey.boxOfficeKey)&targetDt=20120101"
         AF.request(url, method: .get).validate().responseJSON { response in
             switch response.result {
             case .success(let value):
                 let json = JSON(value)
                 print("JSON: \(json)")
+                let name = json["boxOfficeResult"]["dailyBoxOfficeList"][0]["movieNm"].stringValue
+                print(name, "----")
             case .failure(let error):
                 print(error)
             }
