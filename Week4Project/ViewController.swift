3b306a7daf31a7f40bfbbaba2ce8282f74e87bcd
ji-yeon224 <69784492+ji-yeon224@users.noreply.github.com>
2023-08-08 11:55:47 +0900

    230808 수업 - 영화 정보 구조체로 받아서 테이블에 보여주기

--- Week4Project/ViewController.swift
+++ Week4Project/ViewController.swift
@@ -9,10 +9,28 @@ import UIKit
 import SwiftyJSON
 import Alamofire
 
+
+struct Movie {
+    
+    var title: String
+    var release: String
+    
+}
+
 class ViewController: UIViewController {
 
+    @IBOutlet var movieTableView: UITableView!
+    
+    var movieList: [Movie] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        movieTableView.rowHeight = 60
+        
+        movieTableView.dataSource = self
+        movieTableView.delegate = self
+        
         callRequest()
     }
 
@@ -24,15 +42,40 @@ class ViewController: UIViewController {
             switch response.result {
             case .success(let value):
                 let json = JSON(value)
-                print("JSON: \(json)")
-                let name = json["boxOfficeResult"]["dailyBoxOfficeList"][0]["movieNm"].stringValue
-                print(name, "----")
+                let movieInfoList = json["boxOfficeResult"]["dailyBoxOfficeList"]
+                
+                for item in movieInfoList.arrayValue {
+                    self.movieList.append(Movie.init(title: item["movieNm"].stringValue, release: item["openDt"].stringValue))
+                }
+                self.movieTableView.reloadData()
             case .failure(let error):
                 print(error)
             }
         }
         
+        
+        
     }
 
 }
 
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movieList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "movieCell")!
+        
+        cell.textLabel?.text = movieList[indexPath.row].title
+        cell.detailTextLabel?.text = movieList[indexPath.row].release
+        
+        return cell
+    }
+    
+    
+}
