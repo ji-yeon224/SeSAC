cfe957290d674871f8e9bf8aaefcacaa6d03ab59
ji-yeon224 <69784492+ji-yeon224@users.noreply.github.com>
2023-08-16 12:44:13 +0900

    230816 수업 - collection reusable view로 헤더 만들기

---
+++ SeSAC3Week5/View/PosterCollectionViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  PosterCollectionViewCell.swift
+//  SeSAC3Week5
+//
+//  Created by 김지연 on 2023/08/16.
+//
+
+import UIKit
+
+class PosterCollectionViewCell: UICollectionViewCell {
+
+    
+    
+    @IBOutlet var posterImageView: UIImageView!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
