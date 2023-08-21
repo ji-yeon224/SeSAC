3bf9f36c8bf82727e2197118e3b7e316e4873957
ji-yeon224 <69784492+ji-yeon224@users.noreply.github.com>
2023-08-21 11:58:06 +0900

    Initial Commit

---
+++ SeSAC3Week6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSAC3Week6
+//
+//  Created by 김지연 on 2023/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
