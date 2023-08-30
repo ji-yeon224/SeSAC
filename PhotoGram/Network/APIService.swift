affd80ae14e04b29eebcfa76814875d0a7d0dcf3
ji-yeon224 <69784492+ji-yeon224@users.noreply.github.com>
2023-08-30 12:37:52 +0900

    230830 수업 - url session

---
+++ PhotoGram/Network/APIService.swift
@@ -0,0 +1,32 @@
+//
+//  APIService.swift
+//  PhotoGram
+//
+//  Created by 김지연 on 2023/08/30.
+//
+
+import Foundation
+
+class APIService {
+    
+    private init() { }
+    
+    
+    
+    static let shared = APIService() //인스턴스 생성 방지
+    
+    func callRequest() {
+        let url = URL(string: "https://apod.nasa.gov/apod/image/2308/M66_JwstTomlinson_3521.jpg")
+        let request = URLRequest(url: url!)
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            print(data)
+            
+            let value = String(data: data!, encoding: .utf8)
+            print(value)
+            print(response)
+            print(error)
+        }.resume()
+    }
+    
+}
