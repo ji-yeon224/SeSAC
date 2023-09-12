4fd4db959998d4d8b3dc67d1e9679b40788d4bd7
ji-yeon224 <69784492+ji-yeon224@users.noreply.github.com>
2023-09-12 09:37:32 +0900

    230912 수업 - 다국어 대응

--- UnsplashProject/ViewController.swift
+++ UnsplashProject/ViewController.swift
@@ -9,10 +9,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var nicknameTextField: UITextField!
+    
+    @IBOutlet var changeButton: UIButton!
+    
+    @IBOutlet var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        nicknameTextField.placeholder = NSLocalizedString("nickname_placeholder", comment: "")
+        
+//
+//        let value = NSLocalizedString("nickname_result", comment: "")
+//        resultLabel.text = String(format: value, "고래밥", "다마고치", "칙촉")
+        
+        resultLabel.text = "age_result".localized(number: 26)
+        
+        let searchbar = UISearchBar()
+        searchbar.text = "adf"
+        searchbar.placeholder = "asds"
+        
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print(#function)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print(#function)
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print(#function)
+    }
+    
 
 
 }
