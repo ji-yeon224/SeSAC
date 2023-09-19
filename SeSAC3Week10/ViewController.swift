4b832a62e008f035f207d0dbb454d8fb5e0c993c
ji-yeon224 <69784492+ji-yeon224@users.noreply.github.com>
2023-09-19 16:53:17 +0900

    230919 수업 - 네트워크 통신 코드 간결화 시키기

--- SeSAC3Week10/ViewController.swift
+++ SeSAC3Week10/ViewController.swift
@@ -11,9 +11,54 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        
+        
+//        NetworkBasic.shared.random { photo, error in
+//            guard let photo = photo else { return }
+//        }
+        
+//        NetworkBasic.shared.detailPhoto(id: "") { response in
+//            switch response {
+//            case .success(let success):
+//                print(success)
+//            case .failure(let failure):
+//                print(failure)
+//            }
+//        }
+        
+        
+        Network.shared.request(type: PhotoResult.self, api: .photo(id: "JQuR91mrD20")) { response in
+            switch response {
+            case .success(let success):
+                dump(success)
+            case .failure(let failure):
+                print(failure.errorDescription)
+            }
+        }
+        
+        
+        
     }
-
+ 
 
 }
 
+struct Photo: Decodable {
+    let total: Int
+    let total_pages: Int
+    let results: [PhotoResult]
+    
+}
+
+struct PhotoResult: Decodable {
+    let id: String
+    let created_at: String
+    let urls: PhotoURL
+}
+
+struct PhotoURL: Decodable {
+    let full: String
+    let thumb: String
+}
