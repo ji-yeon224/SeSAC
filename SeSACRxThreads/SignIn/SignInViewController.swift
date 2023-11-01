bd033899429d16c8bd6b66be15afc3a949b21671
ji-yeon224 <69784492+ji-yeon224@users.noreply.github.com>
2023-11-01 11:50:21 +0900

    Initial Commit

---
+++ SeSACRxThreads/SignIn/SignInViewController.swift
@@ -0,0 +1,116 @@
+//
+//  SignInViewController.swift
+//  SeSACRxThreads
+//
+//  Created by jack on 2023/10/30.
+//
+
+import UIKit
+import SnapKit
+import RxSwift
+import RxCocoa
+
+class SignInViewController: UIViewController {
+
+    let emailTextField = SignTextField(placeholderText: "이메일을 입력해주세요")
+    let passwordTextField = SignTextField(placeholderText: "비밀번호를 입력해주세요")
+    let signInButton = PointButton(title: "로그인")
+    let signUpButton = UIButton()
+    
+    let test = UISwitch()
+    let isOn = PublishSubject<Bool>()  //BehaviorSubject(value: true)//Observable.of(false)
+    
+    let disposeBag = DisposeBag()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        testSwitch()
+
+        view.backgroundColor = Color.white
+        
+        configureLayout()
+        configure()
+        
+        signUpButton.addTarget(self, action: #selector(signUpButtonClicked), for: .touchUpInside)
+    }
+    
+    func testSwitch() {
+        view.addSubview(test)
+        test.snp.makeConstraints { make in
+            make.top.equalTo(150)
+            make.leading.equalTo(100)
+        }
+        
+        
+        
+        isOn
+            .bind(to: test.rx.isOn)
+            .disposed(by: disposeBag)
+        
+        isOn.onNext(true)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now()+2) {
+            self.isOn.onNext(false)
+        }
+        
+//        isOn
+//            .subscribe { value in
+//                self.test.setOn(value, animated: false)
+//            }
+//            .disposed(by: disposeBag)
+        
+       
+        
+        //UIKit
+//        test.setOn(true, animated: false)
+//        
+//        DispatchQueue.main.asyncAfter(deadline: .now()+2) {
+//            self.test.setOn(false, animated: false) // 2초 후 스위치 꺼짐
+//        }
+        
+    }
+    
+    @objc func signUpButtonClicked() {
+        navigationController?.pushViewController(SignUpViewController(), animated: true)
+    }
+    
+    
+    func configure() {
+        signUpButton.setTitle("회원이 아니십니까?", for: .normal)
+        signUpButton.setTitleColor(Color.black, for: .normal)
+    }
+    
+    func configureLayout() {
+        view.addSubview(emailTextField)
+        view.addSubview(passwordTextField)
+        view.addSubview(signInButton)
+        view.addSubview(signUpButton)
+        
+        emailTextField.snp.makeConstraints { make in
+            make.height.equalTo(50)
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(200)
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
+        }
+        
+        passwordTextField.snp.makeConstraints { make in
+            make.height.equalTo(50)
+            make.top.equalTo(emailTextField.snp.bottom).offset(30)
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
+        }
+        
+        signInButton.snp.makeConstraints { make in
+            make.height.equalTo(50)
+            make.top.equalTo(passwordTextField.snp.bottom).offset(30)
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
+        }
+        
+        signUpButton.snp.makeConstraints { make in
+            make.height.equalTo(50)
+            make.top.equalTo(signInButton.snp.bottom).offset(30)
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
+        }
+    }
+    
+
+}
