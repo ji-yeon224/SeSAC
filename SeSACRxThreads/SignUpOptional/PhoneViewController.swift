e459426dcd412e480b02156e85edfb6ba89eb69e
ji-yeon224 <69784492+ji-yeon224@users.noreply.github.com>
2023-11-01 13:52:27 +0900

    rx assignment - phoneviewcontroller

--- SeSACRxThreads/SignUpOptional/PhoneViewController.swift
+++ SeSACRxThreads/SignUpOptional/PhoneViewController.swift
@@ -7,11 +7,18 @@
  
 import UIKit
 import SnapKit
+import RxSwift
+import RxCocoa
 
 class PhoneViewController: UIViewController {
    
     let phoneTextField = SignTextField(placeholderText: "연락처를 입력해주세요")
     let nextButton = PointButton(title: "다음")
+    
+    let phone = BehaviorSubject(value: "010")
+    let buttonEnabled = BehaviorSubject(value: false)
+    let buttonColor = BehaviorSubject(value: UIColor.lightGray)
+    let disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,7 +28,47 @@ class PhoneViewController: UIViewController {
         configureLayout()
         
         nextButton.addTarget(self, action: #selector(nextButtonClicked), for: .touchUpInside)
+        
+        bind()
     }
+    
+    func bind() {
+        
+        
+        buttonEnabled
+            .bind(to: nextButton.rx.isEnabled)
+            .disposed(by: disposeBag)
+        
+        buttonColor
+            .bind(to: nextButton.rx.backgroundColor)
+            .disposed(by: disposeBag)
+        
+        
+        phone
+            .bind(to: phoneTextField.rx.text)
+            .disposed(by: disposeBag)
+        
+        phoneTextField.rx.text.orEmpty
+            .subscribe(with: self) { owner, value in
+                let result = value.formated(by: "###-####-####")
+                owner.phone.onNext(result)
+            }
+            .disposed(by: disposeBag)
+        
+        
+        
+        phone
+            .map{ $0.count > 10 } // bool값 생성
+            .subscribe(with: self) { owner, value in
+                let color  = value ? UIColor.blue : UIColor.lightGray
+                owner.buttonColor.onNext(color)
+                owner.buttonEnabled.onNext(value)
+            }
+            .disposed(by: disposeBag)
+        
+        
+    }
+    
     
     @objc func nextButtonClicked() {
         navigationController?.pushViewController(NicknameViewController(), animated: true)
