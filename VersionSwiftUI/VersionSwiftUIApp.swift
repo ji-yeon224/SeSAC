ad5e4b46eb8cfd8150ebf4fb87a2f224699d99e1
ji-yeon224 <69784492+ji-yeon224@users.noreply.github.com>
2023-11-23 10:11:39 +0900

    Initial Commit

---
+++ VersionSwiftUI/VersionSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  VersionSwiftUIApp.swift
+//  VersionSwiftUI
+//
+//  Created by 김지연 on 11/23/23.
+//
+
+import SwiftUI
+
+@main
+struct VersionSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
