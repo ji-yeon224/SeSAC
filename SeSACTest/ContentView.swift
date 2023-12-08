967ba32727b0793212a699d1ba35a67a57170b06
ji-yeon224 <69784492+ji-yeon224@users.noreply.github.com>
2023-12-08 10:16:49 +0900

    Initial Commit

---
+++ SeSACTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SeSACTest
+//
+//  Created by 김지연 on 12/8/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
