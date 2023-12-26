217fb2bec99187709d98f45450d25da778486b6a
ji-yeon224 <69784492+ji-yeon224@users.noreply.github.com>
2023-12-26 10:18:59 +0900

    Initial Commit

---
+++ SeSACWebSocket/SeSACWebSocketApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeSACWebSocketApp.swift
+//  SeSACWebSocket
+//
+//  Created by 김지연 on 12/26/23.
+//
+
+import SwiftUI
+
+@main
+struct SeSACWebSocketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
